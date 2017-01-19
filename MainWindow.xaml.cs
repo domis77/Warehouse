60f1e9b0da68890654dfadd898a332811798a880
domis77 <domis77@vp.pl>
2017-01-19 16:09:53 +0100

    fin

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace Magazyn
 {
@@ -20,9 +21,84 @@ namespace Magazyn
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Warehouse.Warehouse warehouse;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            warehouse = new Warehouse.Warehouse();
         }
+
+
+
+        private void addProduct_button_Click(object sender, RoutedEventArgs e)
+        {
+            string name;
+            string description;
+            float price;
+
+            if (name_textBox.Text == "")
+            {
+                System.Windows.MessageBox.Show("Enter name!");
+                return;
+            }
+            else
+            {
+                name = name_textBox.Text;
+            }
+
+            if (description_textBox.Text == "")
+            {
+                System.Windows.MessageBox.Show("Enter description!");
+                return;
+            }
+            else
+            {
+                description = description_textBox.Text;
+            }
+
+            if (price_textBox.Text == "")
+            {
+                System.Windows.MessageBox.Show("Enter price!");
+                return;
+            }
+            else
+            {
+                if (float.TryParse(price_textBox.Text, out price)) { }
+                else
+                {
+                    System.Windows.MessageBox.Show("Entered price is not a number!");
+                    return;
+                }
+            }
+
+            warehouse.addProduct(name, description, price);
+            warehouse.showProducts(addressBook_listView);
+            addressBook_listView.Items.Refresh();
+
+            name_textBox.Text = "";
+            description_textBox.Text = "";
+            price_textBox.Text = "";
+
+            System.Windows.MessageBox.Show("Product added.");
+        }
+
+        private void deleteProduct_button_Click(object sender, RoutedEventArgs e)
+        {
+            Warehouse.Product selectedProduct = (Warehouse.Product)addressBook_listView.SelectedItem;
+            
+            warehouse.deleteProduct(selectedProduct);
+            addressBook_listView.Items.Refresh();
+        }
+
+        private void saveToFile_button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowDialog();
+
+            warehouse.saveToFile(folderBrowserDialog.SelectedPath);
+        }
+
     }
 }
---
+++ Warehouse/Warehouse.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.IO;
+
+namespace Magazyn.Warehouse
+{
+    class Warehouse
+    {
+        private List<Product> productList = new List<Product>();
+
+        public void addProduct(string name, string description, float price)
+        {
+            Product product = new Product(name, description, price);
+            product.id = productList.Count;
+
+            productList.Add(product);
+        }
+
+        public void deleteProduct(Product selectedProduct)
+        {
+            productList.Remove(selectedProduct);
+        }
+
+        public void showProducts(ListView addressBook_listView)
+        {
+            addressBook_listView.ItemsSource = productList; 
+        }
+
+        public void saveToFile(string filePatch)
+        {
+            string file = filePatch + "\\warehouse.txt";
+            try
+            {
+                if ((!File.Exists(filePatch)))
+                {
+                    using(StreamWriter sw = File.CreateText(file))
+                    {
+                        foreach(var p in productList)
+                        {
+                            sw.WriteLine("Id: " + p.id + ", Name: " +  p.name + ", Description: " + p.description + ", Price: " + p.price);
+                        }
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File exist already!");
+                    return;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Failed create file! Orginal Error: " + ex.Message);
+            }
+        }
+    }
+
+
+
+    class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+
+        public Product(string name, string description, float price)
+        {
+            this.name = name;
+            this.description = description;
+            this.price = price;
+        }
+    }
+}
